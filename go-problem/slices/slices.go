031648d1c81ff5a0b5f22b973382f764bf1f6a6d
thinhvoxuan <thinhvoxuan@gmail.com>
2019-05-19 00:31:03 +0700

    tdd with slices

---
+++ go-problem/slices/slices.go
@@ -0,0 +1,31 @@
+package slices
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+	for index, numbers := range numbersToSum {
+		sums[index] = Sum(numbers)
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
